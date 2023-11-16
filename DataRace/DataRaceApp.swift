82c965d64ab7025a44fbc6ba1afbfe1a55c41792
Moroz Pavlo <pavlo@sidekickinteractive.com>
2023-11-16 08:45:00 -0500

    Initial Commit

---
+++ DataRace/DataRaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataRaceApp.swift
+//  DataRace
+//
+//  Created by Moroz Pavlo on 2023-11-16.
+//
+
+import SwiftUI
+
+@main
+struct DataRaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DataRaceUITests/DataRaceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DataRaceUITestsLaunchTests.swift
+//  DataRaceUITests
+//
+//  Created by Moroz Pavlo on 2023-11-16.
+//
+
+import XCTest
+
+final class DataRaceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
